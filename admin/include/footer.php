d48c94d79a8836f623dde13ef542070e899de640
yptse123 <yptse123@gmail.com>
2025-05-10 22:14:30 +0800

    init4

---
+++ admin/include/footer.php
@@ -0,0 +1,12 @@
+    <!-- Mainly scripts -->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+    <!-- Data Tables -->
+    <script src="js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="js/plugins/dataTables/dataTables.fixedColumns.js"></script>
+    <script src="js/plugins/dataTables/dataTables.bootstrap.js"></script>
+    <script src="js/plugins/dataTables/dataTables.responsive.js"></script>
+    <script src="js/plugins/dataTables/dataTables.tableTools.min.js"></script>
+
+    <script src="js/custom.js"></script>
+    
