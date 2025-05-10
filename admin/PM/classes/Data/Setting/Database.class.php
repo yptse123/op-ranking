4dc439703aa99dddc8798836f415512231473d93
mok116 <yeungman2424@gmail.com>
2025-05-10 23:01:10 +0800

    gitignore

--- admin/PM/classes/Data/Setting/Database.class.php
+++ admin/PM/classes/Data/Setting/Database.class.php
@@ -3,9 +3,9 @@
 class PM_Data_Setting_Database
 {
     // Update these constants with the correct Docker settings
-    CONST DB_HOST = "localhost";
+    CONST DB_HOST = "db";
     CONST DB_USERNAME = "root";
-    CONST DB_PASSWORD = "";
+    CONST DB_PASSWORD = "password";
     CONST DB_DBNAME = "op_ranking";
     
     // Add this constant to force TCP/IP protocol
